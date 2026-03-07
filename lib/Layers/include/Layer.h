c05cdf34748e89002ec0a0720f0e7f16c97652e5
Lisha from Linux-Mint-MATE-OLDHP <lishastudies.cs@gmail.com>
2026-03-07 09:00:57 +0530

    Added layer suspension

--- lib/Layers/include/Layer.h
+++ lib/Layers/include/Layer.h
@@ -7,12 +7,20 @@ public:
    Layer(const std::string& name); /// called when new layer pointer made with "new"
    virtual ~Layer() = default; /// called with "delete" keyword
 
+   // ---- Pure virtual functions ----
    virtual void OnAttach() = 0; /// called when layer is actually pushed onto layer stack
    virtual void OnDetach() = 0; /// called when layer is actually popped from layer stack
    virtual void OnUpdate() = 0; /// called every frame (key hold)
    virtual void OnEvent(Event &e) = 0; /// check-once event (key press)
    virtual void OnRender() = 0;
 
+   // ---- layer pause/resume (suspension) ----
+   virtual void OnSuspend() {};
+   virtual void OnResume() {};
+
+   bool isSuspended() const;
+   void setSuspended(bool state);
 protected:
    std::string m_name;
+   bool m_isSuspended;
 };
--- lib/Layers/src/Layer.cpp
+++ lib/Layers/src/Layer.cpp
@@ -2,3 +2,17 @@
 #include <Layer.h>
 
 Layer::Layer(const std::string& name) : m_name(name) {}
+
+bool Layer::isSuspended() const { return m_isSuspended; }
+
+void Layer::setSuspended(bool state) {
+   if(m_isSuspended != state)
+      return; // don't call suspension methods if state didn't change
+
+   m_isSuspended = state;
+
+   if(m_isSuspended)
+      OnSuspend();
+   else
+      OnResume();
+}
